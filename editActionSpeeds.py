fcec8814d0b4b300714a444e0b63cb6286ee5db2
n0237580 <darren.broderick@liberty-it.co.uk>
2021-04-19 11:31:23 +0100

    Format update

--- editActionSpeeds.py
+++ editActionSpeeds.py
@@ -52,8 +52,10 @@ def action_update(action, edit_value):
 # 2nd/3rd/4th Parameter boolean for changing left(positive), straight or right steering angles
 # 5th Parameter is for adding index values
 
+
 # EXAMPLE 1 - Add Speed of 0.5 to all actions, with updating index values
 edit_action_speeds(0.5, True, True, True, True)
+
 
 # EXAMPLE 2 - Minus Speed of 0.1 to all actions, with no change to index values
 # edit_action_speeds(-0.1, True, True, True, False)
