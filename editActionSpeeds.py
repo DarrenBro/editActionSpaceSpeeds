34a3c628702032a0f9477bc90abfd94fd5e04a0b
n0237580 <darren.broderick@liberty-it.co.uk>
2021-05-07 14:53:40 +0100

    Transformed into a class

--- editActionSpeeds.py
+++ editActionSpeeds.py
@@ -2,41 +2,8 @@ import json
 import os
 import uuid
 
-filename = 'action_space.json'
 
-# README
-# Paste in your full action space into the file "action_space.json", it can handle index values if you have them already
-# Run this script with desired parameters on the last line"
-
-
-def edit_action_speeds(edit_value, change_left_steering, change_straight, change_right_steering, add_indexer):
-    with open(filename) as action_space:
-        data = json.load(action_space)
-        index_counter = 0
-        for action in data['action_space']:
-            if add_indexer:
-                action['index'] = index_counter
-                index_counter += 1
-            if change_left_steering:
-                if action['steering_angle'] < 0:
-                    action_update(action, edit_value)
-            if change_straight:
-                if action['steering_angle'] == 0:
-                    action_update(action, edit_value)
-            if change_right_steering:
-                if action['steering_angle'] > 0:
-                    action_update(action, edit_value)
-
-    # Create randomly named temporary file to avoid interference with other thread/asynchronous request
-    tempfile = os.path.join(os.path.dirname(filename), str(uuid.uuid4()))
-    with open(tempfile, 'w') as temp:
-        json.dump(data, temp, indent=4)
-
-    # Rename temporary file replacing old file
-    os.rename(tempfile, filename)
-
-
-def action_update(action, edit_value):
+def load(action, edit_value):
     # Max Speed is 4 so limit there or ignore
     if action['speed'] + edit_value > 4:
         action['speed'] = 4
@@ -48,14 +15,54 @@ def action_update(action, edit_value):
         action['speed'] = round((action['speed'] + edit_value), 2)
 
 
-# 1st Parameter either a positive or negative value for editing all speeds the same value
-# 2nd/3rd/4th Parameter boolean for changing left(positive), straight or right steering angles
-# 5th Parameter is for adding index values
+def update(filename, edit_value, change_left_steering, change_right_steering, add_indexer):
+    with open(filename) as action_space_file:
+        action_space_data = json.load(action_space_file)
+        index_counter = 0
+        for action in action_space_data['action_space']:
+            if add_indexer:
+                action['index'] = index_counter
+                index_counter += 1
+            if change_left_steering:
+                if action['steering_angle'] < 0:
+                    load(action, edit_value)
+            if change_right_steering:
+                if action['steering_angle'] > 0:
+                    load(action, edit_value)
+
+    save(action_space_data, filename)
 
 
-# EXAMPLE 1 - Add Speed of 0.5 to all actions, with updating index values
-edit_action_speeds(0.5, True, True, True, True)
+def save(action_space_data, filename):
+    # Create randomly named temporary file to avoid interference with other thread/asynchronous request
+    temp_file = os.path.join(os.path.dirname(filename), str(uuid.uuid4()))
+
+    with open(temp_file, 'w') as temp:
+        json.dump(action_space_data, temp, indent=4)
+
+    # Rename temporary file replacing old file
+    os.rename(temp_file, filename)
 
 
-# EXAMPLE 2 - Minus Speed of 0.1 to all actions, with no change to index values
-# edit_action_speeds(-0.1, True, True, True, False)
+class UpdateActionSpeeds:
+
+    def __init__(self, filename='action_space.json', edit_value=0.0, change_left_steering=True,
+                 change_right_steering=True, add_indexer=True):
+
+        self.filename = filename
+        self.edit_value = edit_value
+        self.change_left_steering = change_left_steering
+        self.change_right_steering = change_right_steering
+        self.add_indexer = add_indexer
+
+        update(filename, edit_value, change_left_steering, change_right_steering, add_indexer)
+
+
+# Change your parameter values here.
+# edit_value can be positive or negative
+updated_action_speeds = UpdateActionSpeeds(filename='action_space.json', edit_value=0.5, change_left_steering=True,
+                                           change_right_steering=True, add_indexer=True)
+
+
+def run():
+    return updated_action_speeds
